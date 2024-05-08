fa2df3595f7da97d845b7102fe50bc7128fd846a
immanuel-supanova <njugunaimmanuel1@outlook.com>
2024-05-08 11:39:52 +0300

    Saves log traffic into a database

---
+++ middleware/logMiddleware.go
@@ -0,0 +1,75 @@
+package middleware
+
+import (
+	"os"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/immanuel-supanova/go-auth/database"
+	"github.com/immanuel-supanova/go-auth/models"
+	"github.com/mattn/go-isatty"
+)
+
+func LoggerMiddleware(conf gin.LoggerConfig, appName string) gin.HandlerFunc {
+	out := conf.Output
+	if out == nil {
+		out = gin.DefaultWriter
+	}
+
+	notlogged := conf.SkipPaths
+
+	isTerm := true
+
+	if w, ok := out.(*os.File); !ok || os.Getenv("TERM") == "dumb" ||
+		(!isatty.IsTerminal(w.Fd()) && !isatty.IsCygwinTerminal(w.Fd())) {
+		isTerm = false
+	}
+
+	var skip map[string]struct{}
+
+	if length := len(notlogged); length > 0 {
+		skip = make(map[string]struct{}, length)
+
+		for _, path := range notlogged {
+			skip[path] = struct{}{}
+		}
+	}
+
+	return func(c *gin.Context) {
+		// Start timer
+		start := time.Now()
+		path := c.Request.URL.Path
+		raw := c.Request.URL.RawQuery
+
+		// Process request
+		c.Next()
+
+		// Log only when path is not being skipped
+		if _, ok := skip[path]; !ok {
+			param := models.Log{
+				IsTerm: isTerm,
+			}
+
+			// Stop timer
+			param.TimeStamp = time.Now()
+			param.Latency = param.TimeStamp.Sub(start)
+
+			param.ClientIP = c.ClientIP()
+			param.Method = c.Request.Method
+			param.StatusCode = c.Writer.Status()
+			param.ErrorMessage = c.Errors.ByType(gin.ErrorTypePrivate).String()
+
+			param.BodySize = c.Writer.Size()
+
+			if raw != "" {
+				path = path + "?" + raw
+			}
+
+			param.Path = path
+			param.Application = appName
+			// save the data to db
+			database.DB.Create(&param)
+
+		}
+	}
+}
